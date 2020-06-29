1cbf7c92d653acbc0d18027482d690bce0e059ac
zhukangkang <zhukangkang@xinli001.com>
2020-06-29 21:15:56 +0800

    [feat] 初始化项目

---
+++ KKTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KKTest
+//
+//  Created by ZKKMBP on 2020/6/29.
+//  Copyright © 2020 ZKKMBP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
