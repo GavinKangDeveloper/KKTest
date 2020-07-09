35d51b70580f7e31c1d6d0bf3f285d9c6bc66645
zhukangkang <zhukangkang@xinli001.com>
2020-07-09 20:18:11 +0800

    多线程 - 在实际中的应用

--- KKTest/ViewController.m
+++ KKTest/ViewController.m
@@ -24,14 +24,19 @@
 
 @implementation ViewController
 
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     _group = dispatch_group_create();
         
     //第一个参数是优先级，第二个参数保留将来使用。传递除零以外的任何值都可能导致空返回值。
     _globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     self.textV.delegate = self;
-    
 }
 
 - (IBAction)onButtonClicked:(UIButton *)sender {
@@ -40,7 +45,9 @@
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self afterDelay];
+//    [self afterDelay];
+//    [self dispatchOnce];
+    [self changeImageDate];
 }
 
 #pragma mark - 多线程的应用场景
@@ -57,8 +64,66 @@
 
 - (void)sleepAction {
     NSLog(@"-----44---");
-
 }
+
+/// 只执行一次的内容、单例中用的比较多
+- (void)dispatchOnce {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"----dispatch_once_t------");
+    });
+    NSLog(@"----22------");
+}
+
+/// 多任务的依赖
+- (void)groupEnterAndleave {
+  
+    dispatch_group_t group = dispatch_group_create();
+    dispatch_group_enter(group);
+    NSLog(@"");
+    dispatch_group_leave(group);
+     
+    dispatch_group_enter(group);
+    
+    dispatch_group_leave(group);
+
+    //等待 group 中的全部任务执行完毕，才会回到主线程执行这里的任务。
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        //主线程
+       
+    });
+}
+
+- (void)changeImageDate {
+    dispatch_group_t imgGroup = dispatch_group_create();
+    //第一个参数是优先级，第二个参数保留将来使用。传递除零以外的任何值都可能导致空返回值。
+    dispatch_queue_t imgGlobalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    NSDate* startTime = [NSDate date];
+    UIImageJPEGRepresentation([UIImage imageNamed:@"big_img"], 0.5);
+    double deltaTime = [[NSDate date] timeIntervalSinceDate:startTime];
+    NSLog(@"cost time = %f---", deltaTime);//0.173117
+    
+    NSDate* groupTime = [NSDate date];
+    dispatch_group_async(imgGroup, imgGlobalQueue, ^{
+        
+        NSLog(@"-----------111----%@",[NSThread currentThread]);
+        UIImageJPEGRepresentation([UIImage imageNamed:@"big_img"], 0.5);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            double resultTime = [[NSDate date] timeIntervalSinceDate:groupTime];
+            NSLog(@"cost groupTime = %f", resultTime);//0.039186
+        });
+        
+    });
+}
+
+
+
+
+
+
+
+
 
 
 - (void)dispatchGroupAsync {
@@ -87,22 +152,23 @@
 }
 
 - (void)dispatchGroupEnterAndLeave {
-    dispatch_group_enter(_group);
+    dispatch_group_t group = dispatch_group_create();
+    
+    dispatch_group_enter(group);
     NSLog(@"-----------000");
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"-----------111");
-        dispatch_group_leave(_group);
+        dispatch_group_leave(group);
     });
 
-    dispatch_group_enter(_group);
+    dispatch_group_enter(group);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         NSLog(@"-----------222");
-        dispatch_group_leave(_group);
+        dispatch_group_leave(group);
     });
         
-
     //等待 group 中的全部任务执行完毕，才会回到主线程执行这里的任务。
-    dispatch_group_notify(_group, dispatch_get_main_queue(), ^{
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
         //主线程
        NSLog(@"-----------333");
     });
