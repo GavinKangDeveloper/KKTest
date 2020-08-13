b1a773f708d3eb5c8f58a3fae34c2f24132ae2c4
zhukangkang <zhukangkang@yishouapp.com>
2020-08-13 16:54:41 +0800

    新增 防 Crash 的第三方类：AvoidCrash

--- KKTest/AppDelegate.m
+++ KKTest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AvoidCrash.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+//    [AvoidCrash makeAllEffective];
+    
     return YES;
 }
 
