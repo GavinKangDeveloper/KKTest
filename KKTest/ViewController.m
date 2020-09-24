c8db285b8b3f3252732e2ebbca3556564b0127da
zhukangkang <776362265@qq.com>
2020-09-24 21:10:45 +0800

    add：新增比较乱的代码，测试插件：https://www.jianshu.com/p/fd2181fdc567

--- KKTest/ViewController.m
+++ KKTest/ViewController.m
@@ -10,9 +10,9 @@
 #import "KKSecondVC.h"
 
 @interface ViewController ()<UIScrollViewDelegate ,UITextViewDelegate>
-@property (weak, nonatomic) IBOutlet UIButton *btnTemp;
-@property (weak, nonatomic) IBOutlet UITextView *textV;
-@property (weak, nonatomic) IBOutlet UITextField *textF;
+@property (weak, nonatomic) IBOutlet      UIButton *btnTemp;
+@property (weak, nonatomic) IBOutlet UITextView* textV;
+@property (weak, nonatomic) IBOutlet UITextField *    textF;
 
 @end
 
@@ -22,7 +22,42 @@
     [super viewDidLoad];
 
     self.textV.delegate = self;
- 
+
+    [self showParameter:1 parameter1:2 parameter2:3 parameter3parameter3parameter3parameter3parameter3parameter3:4 parameter4:5];
+}
+
+- (void)showParameter:(NSInteger)parameter parameter1:(NSInteger)parameter parameter2:(NSInteger)parameter parameter3parameter3parameter3parameter3parameter3parameter3:(NSInteger)parameter parameter4:(NSInteger)parameter {
+//    注释1
+    int index=0 ;
+    //注释2
+    if (index==1) {
+        
+    }else if (index == 2){//注释3
+        
+    }
+    else if (index == 3) {// 注释4
+        
+    }
+    
+    switch (index) {
+        case 1:{
+            
+        }
+            
+            break;
+            
+            case 2:
+                
+                break;
+            case 3:
+        {
+                
+            }
+                
+                break;
+        default:
+            break;
+    }
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
